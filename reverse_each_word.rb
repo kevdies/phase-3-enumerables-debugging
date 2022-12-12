a6fa4d6e3ac0b893ad211fd221acceeb06a51c85
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-12 14:49:32 -0500

    refactored for cleanliness

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,23 @@
 # Write your code here
+require "pry"
+
+def reverse_each_word(sentence)
+    words = sentence.split(" ")
+    reversed_words = []
+
+    words.each do |word|
+        reversed_words << word.reverse
+    end
+    reversed_words.join(" ")
+end
+
+ reverse_each_word("Hello there, and how are you?")
+
+def reverse_each_word(sentence)
+    sentence.split.map(&:reverse).join(" ")
+end
+
+ reverse_each_word("working on refactoring this code to be so fresh and so clean, clean")
+
+# binding.pry
+# 0
